e14537f025d0d8e3c5915af52ec10506a9575c6e
Nihar Gavva <nihargavva@gmail.com>
2021-01-31 20:01:48 -0500

    learning different loops

--- javascript/basics/index.js
+++ javascript/basics/index.js
@@ -38,6 +38,55 @@ for(var i=0; i<10; i++) {
 }
 
 // while
+var i = 1;
+var numbers = "";
+while (i <= 10)
+{
+  if ( i != 10)
+  {
+    numbers += i + ", ";
+  } else
+  {
+    numbers += i;
+  }
+  i++;
+}
+console.log(numbers);
 // do while
+var i = 0, total = 0;
+do
+{
+  total = total + i;
+  i++;
+}while(i <= 100)
+console.log(total);
 // if else
 // switch
+var days;
+var year = new Date().getFullYear();
+var month = new Date().getMonth();
+//month = 3;
+//month = 1;
+//year = 2020;
+switch (month)
+{
+  case 0:
+  case 2:
+  case 4:
+  case 6:
+  case 7:
+  case 9:
+  case 11:
+    days = 31;
+    break;
+  case 1:
+    if (year % 4 != 0)
+      days = 28;
+    else
+      days = 29;
+    break;
+  default:
+    days = 30;
+}
+console.log(days);
+console.log(month);
