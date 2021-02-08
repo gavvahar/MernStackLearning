53e6fe0a71d72a4e24609e33a7c60b49040d45e8
Nihar Gavva <nihargavva@gmail.com>
2021-02-07 20:15:39 -0500

    testing arrays

--- javascript/arrays/index.js
+++ javascript/arrays/index.js
@@ -2,3 +2,10 @@ var a = [1, 2];
 var aObj = new Array(10);
 
 var arrayObj = [20, 10, 'hello', {x: 50}, true];
+
+var t = [1967, 2015, 2020, 2021];
+var aObj = new Array(2);
+for(var i = 0; i < t.length; i++)
+{
+    console.log(t[i]);
+}
