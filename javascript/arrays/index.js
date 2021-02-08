046f32b3601ea6867ec4b36ed15ee25db624bcdd
Nihar Gavva <nihargavva@gmail.com>
2021-02-07 21:13:07 -0500

    simplifing the code

--- javascript/arrays/index.js
+++ javascript/arrays/index.js
@@ -5,7 +5,4 @@ var arrayObj = [20, 10, 'hello', {x: 50}, true];
 
 var t = [1967, 2015, 2020, 2021];
 var aObj = new Array(2);
-for(var i = 0; i < t.length; i++)
-{
-    console.log(t[i]);
-}
+t.map(x => console.log(x));
