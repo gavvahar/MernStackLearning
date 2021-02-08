78ec808b1f72b9a9e7f0cd1c0c2679fb05972dd0
Nihar Gavva <nihargavva@gmail.com>
2021-02-07 21:01:26 -0500

    testing objects

--- javascript/objects/index.js
+++ javascript/objects/index.js
@@ -10,5 +10,19 @@ var a = {
   z: true,
   dateObj: new Date(),
 };
-
 console.log(a);
+var t =
+{
+  y:1967
+  c: 'Mustang Shelby GT500',
+  obj:
+  {
+    b: 64,
+    abc: function()
+    {
+      console.log(this.b);
+    }
+  },
+  z: true,
+  dateObj: new Date(),
+};
