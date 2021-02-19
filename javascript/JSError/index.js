8f5e884cd5bf2b0db4342fad7528bad0c413dfa4
Nihar Gavva <nihargavva@gmail.com>
2021-02-18 21:01:11 -0500

    testing JSError

--- javascript/JSError/index.js
+++ javascript/JSError/index.js
@@ -1,10 +1,11 @@
 try {
   const a = 10;
   var x = 1;
-  // if(a > 9) {
-  //   throw('MyError', 'number is too big');
-  // } 
-  const dateObj = new Date('sdsdsds');
+   if(a > 9)
+   {
+     throw('MyError', 'number is too big');
+   } 
+  const dateObj = new Date('02/03/2021');/*'sdsdsds'*/
   console.log(a, dateObj.getTime()); 
 } catch (err) {
   console.log(err);
@@ -16,3 +17,20 @@ try {
 
 // var a = 10;
 // console.log(+'30' + a);
+
+try
+{
+  var c = 1967;
+  x = 100;
+  if(c >= x)
+  {
+    throw('MyError', 'number is too big');
+  }
+} catch (err)
+{
+  console.log(err);
+  document.getElementById('error').innerHTML = err.message;
+} finally
+{
+  console.log(c + ' / ' + x + ' = ' + c / x);
+}
