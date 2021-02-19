9e607656a71de8f72038f21b5f95123959629c07
Nihar Gavva <nihargavva@gmail.com>
2021-02-18 21:01:42 -0500

    testing regex

--- javascript/Regex/index.js
+++ javascript/Regex/index.js
@@ -4,3 +4,14 @@ var input = 'Hello world...';
 
 console.log(regexTest.test(input));
 console.log(input.match(regexTest));
+
+
+var car = /Nissan/i;
+var model = 'Nissan GTR';
+console.log(car.test(model));
+console.log(model.match(car));
+
+car = /Ford/s;
+model = 'Ford Mustang';
+console.log(car.test(model));
+console.log(model.match(car));
