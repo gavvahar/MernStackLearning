3239c13874bab40b1756af30943c83a0ed3ad894
harpreet singh <hsit18@gmail.com>
2021-03-26 08:24:15 +0530

    node js basic

---
+++ nodejs/basics/test.js
@@ -0,0 +1,17 @@
+console.log("Hello world...");
+var a =10;
+console.log(a);
+
+
+// myscript.js
+// global.x = 5;
+// setTimeout(() => {
+//   debugger;
+//   console.log('world');
+// }, 1000);
+// console.log('hello');
+
+
+var t = ["Mustang", "Lexus", "Ferrari", "Bugatti"];
+var aObj = new Array(2);
+t.map(x => console.log(x));
