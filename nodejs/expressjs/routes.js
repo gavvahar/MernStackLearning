618dbe4a8b0d56603f0324b79c66c007988aea0d
gavvahar <nihargavva@gmail.com>
2021-04-15 21:06:56 -0400

    hw

--- nodejs/expressjs/routes.js
+++ nodejs/expressjs/routes.js
@@ -13,7 +13,7 @@ app.get("/user", function (req, res) {
 });
 app.get("/80eighty", function (req, res) {
   console.log("GET Request received for /user URI");
-  res.send(`<h2 style="font-family: Malgun Gothic; color:blue;">GET Request received for 80Eighty URI!!!</h2>`);
+  res.send(`<h2 style="font-family: Malgun Gothic; color:blue;">Nissan GTR</h2>`);
 });
 // fetch('http://localhost:8080/adduser', {method: 'POST'}).then(res => console.log(res))
 app.post("/adduser", function (req, res) {
