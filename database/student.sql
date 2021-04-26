445155c996d0ad62aa87de107b8f2579d4afe8b0
harpreet singh <hsit18@gmail.com>
2021-04-26 08:16:03 +0530

    student mysql commands

---
+++ database/student.sql
@@ -0,0 +1,5 @@
+create table student (id integer, firstname varchar(100), lastname varchar(100), subject varchar(100));
+insert into student (id, firstname, lastname, subject) values (1, 'albert', 'einstein', 'Science');
+insert into student (id, firstname, lastname, subject) values (2, 'isaac', 'newton', 'History');
+insert into student (id, firstname, lastname, subject) values (3, 'marie', 'curie', 'Mathematics');
+select * from student;
